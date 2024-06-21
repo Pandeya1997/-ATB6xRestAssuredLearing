ec0923a562ed67b3a1371e01460dd704e9f74efc
abhishek pandey <abhishekpy55@gmail.com>
2024-06-21 23:17:02 +0530

    Testng project added

---
+++ src/test/java/Testng1/Lab261.java
@@ -0,0 +1,30 @@
+package Testng1;
+
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class Lab261 {
+    //100 -Group these test case
+    //smoke sanity
+    // Before , After we can do sometime
+@Description("TC#1 - Verify GET 261 Request 1 ")
+    @Test
+    public void getRequest(){
+      RestAssured
+        .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when().get().then().statusCode( 201);
+}
+    @Description("TC#1 - Verify GET 261 Request 2 ")
+    @Test
+    public void getRequest2(){
+      RestAssured
+        .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when().get()
+              .then().statusCode( 201);
+    }
+}
