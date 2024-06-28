b89807e29fe9dd805350456b52e526f18c262e06
abhishek pandey <abhishekpy55@gmail.com>
2024-06-28 08:15:38 +0530

    jsonBodyUsingMap

---
+++ src/test/java/com/thetestingacademy/ex_27_06_2024/Lab289.java
@@ -0,0 +1,10 @@
+package com.thetestingacademy.ex_27_06_2024;
+
+public class Lab289 {
+    // Payload Using Map
+    // Data Driven Testing via Excel
+    // Serialization and DeSerialization
+    // GSON and Jackson API
+    // JSON Schema Validation
+
+}
---
+++ src/test/java/com/thetestingacademy/ex_27_06_2024/gson/jacksonapi/Lab291.java
@@ -0,0 +1,4 @@
+package com.thetestingacademy.ex_27_06_2024.gson.jacksonapi;
+
+public class Lab291  {
+}
